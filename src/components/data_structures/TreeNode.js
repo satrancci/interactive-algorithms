b506fbbd32da5f4e9f97519b8b7c10fb067d8e22
satrancci <thescarface1993@gmail.com>
2021-03-10 13:54:22 -0600

    added random ID to each node

--- src/components/data_structures/TreeNode.js
+++ src/components/data_structures/TreeNode.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Group, Circle, Text } from 'react-konva';
+import { Group, Circle, Line, Text } from 'react-konva';
 
 
 const TreeNode = (props) => {
@@ -9,11 +9,16 @@ const TreeNode = (props) => {
     console.log('TREE NODE: i:', i, 'x:', x, 'y:', y, 'val:', val, 'level:', level);
 
     const fontSize = 15;
+    const radius = 30;
+
+    const nodeID = Math.random().toString(36).substring(2,3)+Math.random().toString(36).substring(2,3);
 
     return (
-        <Group>
-             <Circle x={x} y={y} radius={30} fill="green" />
-             <Text id={"node"+i} text={val} x={x} y={y} fontSize={fontSize} />
+        <Group id={"nodeID"+nodeID}>
+             <Circle x={x} y={y} radius={radius} fill="green" />
+             <Line points={[x-radius, y+radius*0.3, x+radius, y+radius*0.3]} stroke="black" strokeWidth={0.5} />
+             <Text text={val} x={x} y={y-radius*0.3} fontSize={fontSize} />
+             <Text text={nodeID} x={x} y={y+radius*0.5} fontSize={fontSize*0.75} />
         </Group>
         
     );

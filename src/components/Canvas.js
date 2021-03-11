9d1d99514589c9efc02abc939d5110f4f567ef2a
satrancci <thescarface1993@gmail.com>
2021-03-11 02:38:27 -0600

    pass treeValues.root from redux store as prop down to Tree and TreeNode components

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -63,6 +63,7 @@ const Canvas = (props) => {
             windowHeight={windowHeight}
             windowCenter={windowCenter}
             fontSize={fontSize}
+            treeValues={props.state.treeValues.root}
           />
         }
         {true ? (

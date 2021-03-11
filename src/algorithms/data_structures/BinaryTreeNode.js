458f3257626977ec0c864235a1446bb9f9154d44
satrancci <thescarface1993@gmail.com>
2021-03-11 10:54:42 -0600

    added parentID property to BinaryTreeNode class

--- src/algorithms/data_structures/BinaryTreeNode.js
+++ src/algorithms/data_structures/BinaryTreeNode.js
@@ -2,6 +2,7 @@ export default class BinaryTreeNode {
   constructor(id, value) {
     this.id = id;
     this.value = value;
+    this.parentID = null;
     this.left = null;
     this.right = null;
   }

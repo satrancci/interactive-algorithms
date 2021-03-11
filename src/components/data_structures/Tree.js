eac3bfa7b5b7216a3365b3ae082202d329acee3f
satrancci <thescarface1993@gmail.com>
2021-03-10 22:51:17 -0600

    change rendering logic, values is now an object rather than an array

--- src/components/data_structures/Tree.js
+++ src/components/data_structures/Tree.js
@@ -1,52 +1,41 @@
-import React, { useEffect } from 'react';
-import { Group, Rect, Text } from 'react-konva';
+import React from 'react';
+import { Group } from 'react-konva';
 
 import TreeNode from './TreeNode';
 
 
 const Tree = (props) => {
-    const {values, x, offset, level, isRoot, windowCenter} = props;
+    const {values, x, offset, level, isRoot, windowCenter, child} = props;
 
+    /*
+    console.log('values received:', values);
+    console.log('child:', child);
+    console.log('x:', x);
+    console.log('level:', level);
+    console.log('offset:', offset);
+    console.log('windowCenter:', windowCenter);
+    */
 
-    if (!values || !values.length) {
-        console.log('returning');
+    if (!values) {
         return null;
     }
 
-    
-    //console.log('isRoot:', isRoot);
-    //console.log('x:', x);
-    console.log('values received:', values);
-    console.log('level:', level);
-    //console.log('offset:', offset);
 
     const levelOffset = 2**(level+1);
-    console.log('levelOffset:', levelOffset);
+    //console.log('levelOffset:', levelOffset);
     const y = 50;
 
-    console.log('windowCenter:', windowCenter);
-    const distOffset = windowCenter*1.5/levelOffset;
-    console.log('distOffset:', distOffset);
+    
+    const distOffset = windowCenter/levelOffset;
+    //console.log('distOffset:', distOffset);
 
     return (
         <Group>
-        {values.map((val, i) => {
-            return (
-                <Group>
-                    {/*console.log(`mapping over... val.value: ${val.value}, i: ${i}`) */}
-
-                    {/* Drawing current node */}
-                    {isRoot ? <TreeNode i={i} x={x} y={y+level*offset} level={level} val={val.value} parentX={null} parentY={null} /> : null}
-                    {(!isRoot) && (i === 0) ? <TreeNode i={i} x={x-distOffset} y={y+level*offset} level={level} val={val.value} parentX={x} parentY={y+(level-1)*offset}/> : null}
-                    {(!isRoot) && (i === 1) ? <TreeNode i={i} x={x+distOffset} y={y+level*offset} level={level} val={val.value} parentX={x} parentY={y+(level-1)*offset}/> : null}
-                    {/* Recursing */} 
-                    {/*<Tree values={val.children} offset={offset} level={level+1} x={x/2} isRoot={false} />*/}
-                    {isRoot ? <Tree values={val.children} offset={offset} level={level+1} x={x} isRoot={false} windowCenter={windowCenter}/> : null }
-                    {(!isRoot) && (i === 0) ? <Tree values={val.children} offset={offset} level={level+1} x={x-distOffset} isRoot={false} windowCenter={windowCenter} /> : null }
-                    {(!isRoot) && (i === 1) ? <Tree values={val.children} offset={offset} level={level+1} x={x+distOffset} isRoot={false} windowCenter={windowCenter} /> : null }
-                </Group>
-            )
-        })}
+        {isRoot ? <TreeNode x={x} y={y+level*offset} id={values.id} level={level} val={values.value} parentX={null} parentY={null} /> : null}
+        {child === "left" ? <TreeNode x={x} y={y+level*offset} id={values.id} level={level} val={values.value} parentX={x+distOffset*2} parentY={y+(level-1)*offset}/> : null}
+        {child === "right"? <TreeNode x={x} y={y+level*offset} id={values.id} level={level} val={values.value} parentX={x-distOffset*2} parentY={y+(level-1)*offset}/> : null}
+        {values.left ? <Tree values={values.left} offset={offset} level={level+1} x={x-distOffset} isRoot={false} windowCenter={windowCenter} child="left" /> : null}
+        {values.right? <Tree values={values.right} offset={offset} level={level+1} x={x+distOffset} isRoot={false} windowCenter={windowCenter} child="right" /> : null}
         </Group>
     )
 }

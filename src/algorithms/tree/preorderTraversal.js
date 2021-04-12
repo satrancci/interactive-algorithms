35323d286895ff10eff420089e5ee56cf4c6920b
satrancci <thescarface1993@gmail.com>
2021-04-11 23:21:04 -0500

    integrate visualizationSpeed with preorderTraversal

--- src/algorithms/tree/preorderTraversal.js
+++ src/algorithms/tree/preorderTraversal.js
@@ -14,43 +14,44 @@ const preorderTraversalHelper = async (node) => {
     if (!node) {
         return;
     }
+    const BASE_SLEEP_TIME = 700;
 
     store.dispatch(updateNodeID(node.id));
     store.dispatch(updateMessage(`Exploring node ${node.id}`));
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
     store.dispatch(updateMessage(`Check left child of ${node.id}`));
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
     
     if (node.left) {
         store.dispatch(updateMessage(`Left child of ${node.id} exists. Going there...`));
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
         await preorderTraversalHelper(node.left);
 
         store.dispatch(updateMessage(`Recursing...`));
         store.dispatch(updateNodeID(node.id));
-        await new Promise((r) => setTimeout(r, 1000));
+        await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
     } else {
         store.dispatch(updateMessage(`Left child of ${node.id} does not exist.`));
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
     }
 
     store.dispatch(updateMessage(`Check right child of ${node.id}`));
-    await new Promise((r) => setTimeout(r, 2000));
+    await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
     if (node.right) {
         store.dispatch(updateMessage(`Right child of ${node.id} exists. Going there...`));
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
         await preorderTraversalHelper(node.right);
     } else {
         store.dispatch(updateMessage(`Right child of ${node.id} does not exist.`));
-        await new Promise((r) => setTimeout(r, 2000));
+        await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
     }
 
     store.dispatch(updateMessage(`Recursing...`));
     store.dispatch(updateNodeID(node.id));
-    await new Promise((r) => setTimeout(r, 1000));
+    await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
 }
 

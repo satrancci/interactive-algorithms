5174018ce4e0f2956784c6732cba772204d77888
satrancci <thescarface1993@gmail.com>
2021-04-15 15:46:12 -0500

    adapt longestIncreasingSubsequence() to the new design

--- src/algorithms/array2d/longestIncreasingSubsequence.js
+++ src/algorithms/array2d/longestIncreasingSubsequence.js
@@ -1,22 +1,20 @@
 import store from "../../store";
-import { addValue, updateIndex, updateAtIndex, updateMessage } from "../../actions";
+import { updateIndex, updateAtIndex, updateMessage, assignVisValues } from "../../actions";
+import _ from "lodash";
 
 
 const longestIncreasingSubsequence = async (paramsObj) => {
 
     const BASE_SLEEP_TIME = 700;
-    // get values from paramsObj later, for now hard-coded
-    const values = [10,9,2,5,3,7,101,18];
 
-    const n = values.length;
+    const {arr} = store.getState().inputObj;
+    console.log(`longestIncreasingSubsequence received arr: ${arr}`);
+
+    const n = arr.length;
 
     const dp = new Array(n).fill(1);
-    for (let i = 0; i < dp.length; i++) {
-        store.dispatch(addValue(dp[i]));
-    };
 
-    store.dispatch(updateMessage(`For now, dp algorithms runs on the hard-coded values=[${values}]`));
-    await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
+    store.dispatch(assignVisValues(_.cloneDeep(dp)));
 
     let maxLength = 1;
 
@@ -29,15 +27,16 @@ const longestIncreasingSubsequence = async (paramsObj) => {
             store.dispatch(updateMessage(`prevIdx: ${prevIdx}`));
             await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
-            store.dispatch(updateMessage(`Check values[curIdx] > values[prevIdx] && dp[curIdx] <= dp[prevIdx]:  ${values[curIdx]}>${values[prevIdx]} && ${dp[curIdx]}<=${dp[prevIdx]}`));
+            store.dispatch(updateMessage(`Check arr[curIdx] > arr[prevIdx] && dp[curIdx] <= dp[prevIdx]:  ${arr[curIdx]}>${arr[prevIdx]} && ${dp[curIdx]}<=${dp[prevIdx]}`));
             await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
-            if (values[curIdx] > values[prevIdx] && dp[curIdx] <= dp[prevIdx]) {
-                store.dispatch(updateMessage(`values[curIdx] > values[prevIdx] && dp[curIdx] <= dp[prevIdx] evaluated to TRUE`));
+            if (arr[curIdx] > arr[prevIdx] && dp[curIdx] <= dp[prevIdx]) {
+                store.dispatch(updateMessage(`arr[curIdx] > arr[prevIdx] && dp[curIdx] <= dp[prevIdx] evaluated to TRUE`));
                 await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
                 store.dispatch(updateMessage(`Set dp[curIdx] = dp[prevIdx] + 1;`));
                 dp[curIdx] = dp[prevIdx] + 1;
+                store.dispatch(assignVisValues(_.cloneDeep(dp)));
                 store.dispatch(updateAtIndex(curIdx, dp[curIdx]));
                 await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
 
@@ -48,7 +47,7 @@ const longestIncreasingSubsequence = async (paramsObj) => {
         }
     }
 
-    store.dispatch(updateMessage(`Completed. Length of LIS for values=[${values}] is ${maxLength}.`));
+    store.dispatch(updateMessage(`Completed. Length of LIS for arr=[${arr}] is ${maxLength}.`));
     await new Promise((r) => setTimeout(r, BASE_SLEEP_TIME * store.getState().visualizationSpeed));
     return maxLength;
     

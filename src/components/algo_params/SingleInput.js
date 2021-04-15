8ba41566b57b3e9356507a95d7d3be56e312a28e
satrancci <thescarface1993@gmail.com>
2021-04-15 02:22:24 -0500

    add placeholders based on inputName, split arr values by comma

--- src/components/algo_params/SingleInput.js
+++ src/components/algo_params/SingleInput.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 import { Input, Button } from 'semantic-ui-react';
 
+const getPlaceholderValue = (inputName) => {
+    switch (inputName) {
+        case "arr":
+            return "e.g. 1,2,3";
+        case "targetSum":
+        case "n":
+            return "e.g. 5";
+        case "str1":
+        case "str2":
+            return "e.g. abc"
+        default:
+            return "";
+    }
+}
+
+
 const SingleInput = (props) => {
   
   const inputName = props.inputName;
+  const placeholderValue = getPlaceholderValue(inputName);
+
   const [valToAdd, setValToAdd] = useState("");
 
   const onAddValue = new_val => setValToAdd(new_val);
@@ -11,14 +29,21 @@ const SingleInput = (props) => {
   const onAddSubmit = () => {
     // need to add validation!
     if (valToAdd) {
-      props.onSingleInputSubmit(inputName, valToAdd);
-      //setValToAdd("");
+        if (inputName === "arr") {
+            // just for now
+            const arr = valToAdd.split(",");
+            props.onSingleInputSubmit(inputName, arr);
+        } else {
+            props.onSingleInputSubmit(inputName, valToAdd);
+        }
+    //setValToAdd("");
     }
   };
 
     return (
       <div style={{display: "inline-block", marginRight: "5px", marginBottom: "10px"}}>
         <Input
+          placeholder={placeholderValue}
           label={`${inputName}: `}
           id={inputName}
           type="text"

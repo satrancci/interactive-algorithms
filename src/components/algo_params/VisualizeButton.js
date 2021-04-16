40ffe447b4b030c0b2b22c875a4787e1cff6d3a3
satrancci <thescarface1993@gmail.com>
2021-04-16 02:10:04 -0500

    minor cleanup

--- src/components/algo_params/VisualizeButton.js
+++ src/components/algo_params/VisualizeButton.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { connect } from "react-redux";
 import { Button } from 'semantic-ui-react';
 import algorithmMappings from '../../algorithmMappings';
@@ -6,21 +6,14 @@ import algorithmMappings from '../../algorithmMappings';
 const VisualizeButton = (props) => {
 
   const onVisualize = async () => {
-    if (!(props.state["dataStructure"] && props.state["algorithm"])) { // add another check for params!
+    if (!(props.state["dataStructure"] && props.state["algorithm"])) {
       console.log("You must select ds and algorithm");
       return;
     }
     console.log("onVisualize clicked!");
     
-    console.log('params received by onVisualized!:', props.state.params);
     const f = algorithmMappings[props.state.algorithm].f;
-    // find a way to apply arguments in a sorted order!!!
-    //f.apply(null, [props.state.values, Object.values(props.state.params)]); // doesn't work right now
-    let allParameters = Object.keys(props.state.params).length > 0 ? Object.fromEntries(props.state.params) : {}; // needs to be improved!!
-    allParameters["values"] = props.state.values;
-    allParameters["tree"] = props.state.treeValues;
-    console.log('OnVisualize() is sending:', allParameters);
-    await f(allParameters);
+    await f();
     };
 
 

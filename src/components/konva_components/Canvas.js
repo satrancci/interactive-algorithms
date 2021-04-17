2803c94c2b350c3d9b683d8040e92a6afc2ae79c
satrancci <thescarface1993@gmail.com>
2021-04-17 03:35:25 -0500

    gets sizes as props from App component, Stage is draggable, other minor changes

--- src/components/konva_components/Canvas.js
+++ src/components/konva_components/Canvas.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import { Stage, Layer, Circle } from "react-konva";
+import { Stage, Layer } from "react-konva";
 import PrimaryDataStructure from "./data_structures/PrimaryDataStructure";
 import DynamicIndex from "./explanations/DynamicIndex";
 import Message from "./explanations/Message";
@@ -8,88 +8,86 @@ import AuxiliaryInfo from "./explanations/AuxiliaryInfo";
 
 const Canvas = (props) => {
 
-  const scaleFactorX = 0.8;
-  const scaleFactorY = 0.8;
 
-  const [windowWidth, setWindowWidth] = useState(
-    window.innerWidth * scaleFactorX
-  );
-  const [windowHeight, setWindowHeight] = useState(
-    window.innerHeight * scaleFactorY
-  );
+  console.log(`Canvas received props.x: ${props.x}`);
+  const availableWidth = window.innerWidth;
+  console.log(`Canvas available width: ${availableWidth}`);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth * scaleFactorX);
-      setWindowHeight(window.innerHeight * scaleFactorY);
-    };
-    window.addEventListener("resize", handleResize);
+  console.log(`Canvas received props.y: ${props.y}`);
+  const availableHeight = window.innerHeight - props.y;
+  console.log(`Canvas available height: ${availableHeight}`);
 
-    return (_) => {
-      window.removeEventListener("resize", handleResize);
-    };
-  });
+  const canvasWidth = availableWidth * 0.9;
+  const canvasHeight = availableHeight * 0.9;
+  console.log(`CANVAS SIZE: ${canvasWidth}x${canvasHeight}`);
 
   // for Array
   const N = props.state.visValues.length;
   const divisor = Math.max(N, 5);
-  const cellX = windowWidth*0.1;
-  const cellWidth = windowWidth/(divisor*1.2);
-  const cellHeight = windowHeight/(divisor*1.2);
-  const iFactor = windowWidth/(divisor*1.2);
-  const textCenterX = cellX+cellWidth*0.5;
-  const fontSize = cellWidth/4;
+  const cellX = canvasWidth * 0.1;
+  const cellWidth = canvasWidth / (divisor * 1.2);
+  const cellHeight = canvasHeight / (divisor * 1.2);
+  const iFactor = canvasWidth / (divisor * 1.2);
+  const textCenterX = cellX + cellWidth * 0.5;
+  const fontSize = cellWidth / 4;
 
-  const windowCenter = windowWidth/2;
-  
+  console.log(`cellX: ${cellX}, cellWidth: ${cellWidth}, cellHeight: ${cellHeight}`)
+
+  const canvasCenter = canvasWidth / 2;
 
   return (
-    <Stage
-      width={windowWidth}
-      height={windowHeight}
-      style={{ borderStyle: "solid" }}
+    <div
+      id="canvas-wrapper"
+      style={{
+        overflow: "auto",
+        boxSizing: "border-box",
+        padding: "2%",
+        width: availableWidth,
+        height: availableHeight
+      }}
     >
-      <Layer>
-        {
-          <PrimaryDataStructure
-            values={props.state.visValues}
-            treeValues={props.state.visValues.root || props.state.treeValues.root}
-            dataStructure={props.state.dataStructure}
-            indices={props.state.indices}
-            cellX={cellX}
-            cellWidth={cellWidth}
-            cellHeight={cellHeight}
-            iFactor={iFactor}
-            textCenterX={textCenterX}
-            windowWidth={windowWidth}
-            windowHeight={windowHeight}
-            windowCenter={windowCenter}
-            fontSize={fontSize}
-            curNodeID={props.state.nodeID}
-          />
-        }
-        {props.state.dataStructure === "Array" ? (
-          <DynamicIndex
-            indices={props.state.indices}
-            textCenterX={textCenterX}
-            windowHeight={windowHeight}
-            cellHeight={cellHeight}
-            iFactor={iFactor}
-            cellWidth={cellWidth}
-            fontSize={fontSize}
-
-          />
-        ) : null}
-        {
-          <Message
-            message={props.state.message}
-            windowWidth={windowWidth}
-            windowHeight={windowHeight}
-          />
-        }
-        {<AuxiliaryInfo />}
-      </Layer>
-    </Stage>
+      <Stage width={canvasWidth} height={canvasHeight} draggable>
+        <Layer>
+          {
+            <PrimaryDataStructure
+              values={props.state.visValues}
+              treeValues={props.state.visValues.root || props.state.treeValues.root}
+              dataStructure={props.state.dataStructure}
+              indices={props.state.indices}
+              cellX={cellX}
+              cellWidth={cellWidth}
+              cellHeight={cellHeight}
+              iFactor={iFactor}
+              textCenterX={textCenterX}
+              canvasWidth={canvasWidth}
+              canvasHeight={canvasHeight}
+              canvasCenter={canvasCenter}
+              fontSize={fontSize}
+              curNodeID={props.state.nodeID}
+            />
+          }
+          {props.state.dataStructure === "Array" ? (
+            <DynamicIndex
+              indices={props.state.indices}
+              textCenterX={textCenterX}
+              canvasHeight={canvasHeight}
+              cellHeight={cellHeight}
+              iFactor={iFactor}
+              cellWidth={cellWidth}
+              fontSize={fontSize}
+            />
+          ) : null}
+          {
+            <Message
+              message={props.state.message}
+              canvasWidth={canvasWidth}
+              canvasHeight={canvasHeight}
+            />
+          }
+          {<AuxiliaryInfo />}
+        </Layer>
+      </Stage>
+    </div>
   );
 };
 

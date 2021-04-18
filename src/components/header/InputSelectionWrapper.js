092018766e31393f38b41ba3bcd7754342172ae4
satrancci <thescarface1993@gmail.com>
2021-04-17 23:24:38 -0500

    remove AlgorithmSelection and VisualizeButton from the InputSelectionWrapper component

--- src/components/header/InputSelectionWrapper.js
+++ src/components/header/InputSelectionWrapper.js
@@ -2,9 +2,6 @@ import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
 
-import VisualizeButton from "../algo_params/VisualizeButton";
-import DataStructureSelection from "../ds_algo_selection/DataStructureSelection";
-import AlgorithmSelection from "../ds_algo_selection/AlgorithmSelection";
 import VisualizationSpeed from "../algo_params/VisualizationSpeed";
 import InputParams from "../algo_params/InputParams";
 import DebuggingState from "../debugging/DebuggingState";
@@ -24,15 +21,10 @@ const InputSelectionWrapper = (props) => {
 
   return (
     <div id="input-selection-wrapper" style={{boxSizing: "border-box", backgroundColor: "#72ce9b", padding: "0%"}} ref = {elemRef}>
-      <AlgorithmSelection />
 
-      {props.state.algorithm ? <DataStructureSelection disabled={false}/> : <DataStructureSelection disabled={true}/>}
-
-      {props.state.algorithm && props.state.dataStructure && <InputParams/>}
+      {props.state.algorithm && <InputParams/>}
       
-      {props.state.algorithm && props.state.dataStructure && (!_.isEmpty(props.state.inputObj)) && <VisualizationSpeed/>}
-
-      {props.state.algorithm && props.state.dataStructure && (!_.isEmpty(props.state.inputObj)) && <VisualizeButton />}
+      {props.state.algorithm && (!_.isEmpty(props.state.inputObj)) && <VisualizationSpeed/>}
 
       {/*<DebuggingState />*/}
     </div>

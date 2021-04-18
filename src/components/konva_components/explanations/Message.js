af212532c62c7a54734a8f50d849be92ca91e099
satrancci <thescarface1993@gmail.com>
2021-04-18 13:31:23 -0500

    make Message draggable, change x- and y-positions

--- src/components/konva_components/explanations/Message.js
+++ src/components/konva_components/explanations/Message.js
@@ -3,13 +3,13 @@ import { Group, Text } from 'react-konva';
 
 const Message = (props) => {
 
-    const textStartX = props.canvasWidth*0.1;
-    const textStartY = props.canvasHeight*0.5;
+    const textStartX = props.canvasWidth*0.02;
+    const textStartY = props.canvasHeight*0.85;
 
     const fontSize = props.canvasWidth/40;
 
     return (
-        <Group>
+        <Group draggable>
             {props.message ? <Text id={"message"} text={props.message} x={textStartX} y={textStartY} fontSize={fontSize}/> : null}
         </Group>
     )

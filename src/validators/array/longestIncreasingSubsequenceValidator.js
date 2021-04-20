30649e53d03c864645bf3e3374a5281fbe5c1f12
satrancci <thescarface1993@gmail.com>
2021-04-20 13:11:46 -0500

    add LIS validator

--- src/validators/array/longestIncreasingSubsequenceValidator.js
+++ src/validators/array/longestIncreasingSubsequenceValidator.js
@@ -1,5 +1,25 @@
-const longestIncreasingSubsequenceValidator = () => {
-    return true;
-}
+const Joi = require("joi");
+
+const schema = {
+  arr: Joi.object({
+    arr: Joi.array().min(1).max(15).required().items(Joi.number())
+  })
+};
+
+
+
+const longestIncreasingSubsequenceValidator = (inputName, value) => {
+  //console.log(`longestIncreasingSubsequenceValidator received inputName: ${inputName}, value: ${value}`);
+  let res = null;
+  switch (inputName) {
+      case "arr":
+        let a = value.split(",");
+        a = a.length === 1 && a[0] === "" ? [] : a;
+        res = schema[inputName].validate({ [inputName]: a});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      default:
+        return;
+  }
+};
 
 export default longestIncreasingSubsequenceValidator;

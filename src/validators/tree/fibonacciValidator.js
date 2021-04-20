1e34913341da688202caf2df70aaa129dd1fb6a4
satrancci <thescarface1993@gmail.com>
2021-04-20 13:31:00 -0500

    add fibonacci validator

--- src/validators/tree/fibonacciValidator.js
+++ src/validators/tree/fibonacciValidator.js
@@ -1,5 +1,23 @@
-const fibonacciValidator = () => {
-    return true;
-}
+const joi = require("joi");
 
-export default fibonacciValidator;
+const schema = {
+  n: joi.object({
+    n: joi.number().min(0).max(6).required(),
+  })
+};
+
+
+
+const fibonaccivalidator = (inputName, value) => {
+  console.log(`fibonacciValidator received inputname: ${inputName}, value: ${value}`);
+  let res = null;
+  switch (inputName) {
+      case "n":
+        res = schema[inputName].validate({ [inputName]: value});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      default:
+        return;
+  }
+};
+
+export default fibonaccivalidator;

f1277c26753dcf5be1a66431057c29da46447a27
satrancci <thescarface1993@gmail.com>
2021-04-20 13:04:10 -0500

    add isPalindrome validator

--- src/validators/array/isPalindromeValidator.js
+++ src/validators/array/isPalindromeValidator.js
@@ -1,5 +1,24 @@
-const isPalindromeValidator = () => {
-    return true;
-}
+const Joi = require("joi");
+
+const schema = {
+  arr: Joi.object({
+    arr: Joi.array().min(1).max(15).required().items(Joi.string().alphanum())
+  })
+};
+
+
+const isPalindromeValidator = (inputName, value) => {
+  //console.log(`isPalindromeValidator received inputName: ${inputName}, value: ${value}`);
+  let res = null;
+  switch (inputName) {
+      case "arr":
+        let a = value.split(",");
+        a = a.length === 1 && a[0] === "" ? [] : a;
+        res = schema[inputName].validate({ [inputName]: a});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      default:
+        return;
+  }
+};
 
 export default isPalindromeValidator;

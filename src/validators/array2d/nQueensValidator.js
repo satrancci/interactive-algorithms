157073e2b8ecd042cc02928644dd1793523c588e
satrancci <thescarface1993@gmail.com>
2021-04-20 13:35:02 -0500

    add nQueens validator

--- src/validators/array2d/nQueensValidator.js
+++ src/validators/array2d/nQueensValidator.js
@@ -1,5 +1,23 @@
-const nQueensValidator = () => {
-    return true;
-}
+const joi = require("joi");
+
+const schema = {
+  n: joi.object({
+    n: joi.number().min(2).max(8).required(),
+  })
+};
+
+
+
+const nQueensValidator = (inputName, value) => {
+  console.log(`nQueensValidator received inputname: ${inputName}, value: ${value}`);
+  let res = null;
+  switch (inputName) {
+      case "n":
+        res = schema[inputName].validate({ [inputName]: value});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      default:
+        return;
+  }
+};
 
 export default nQueensValidator;

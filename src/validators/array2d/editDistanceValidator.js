c68d7d32044320099afa6c990a6337987b777af5
satrancci <thescarface1993@gmail.com>
2021-04-20 14:17:33 -0500

    add editDistance validator

--- src/validators/array2d/editDistanceValidator.js
+++ src/validators/array2d/editDistanceValidator.js
@@ -1,5 +1,28 @@
-const editDistanceValidator = () => {
-    return true;
-}
+const joi = require("joi");
 
+const schema = {
+  str1: joi.object({
+    str1: joi.string().min(1).max(8).regex(/^[a-z]+$/).required(),
+  }),
+  str2: joi.object({
+    str2: joi.string().min(1).max(8).regex(/^[a-z]+$/).required(),
+  }),
+};
+
+
+
+const editDistanceValidator = (inputName, value) => {
+  //console.log(`editDistanceValidator received inputname: ${inputName}, value: ${value}`);
+  let res = null;
+  switch (inputName) {
+      case "str1":
+        res = schema[inputName].validate({ [inputName]: value});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      case "str2":
+        res = schema[inputName].validate({ [inputName]: value});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      default:
+        return;
+  }
+};
 export default editDistanceValidator;

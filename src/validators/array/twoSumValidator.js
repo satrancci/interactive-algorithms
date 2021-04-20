8daf4d00c6742455f5f04a2d87cd74e4d5231f84
satrancci <thescarface1993@gmail.com>
2021-04-20 00:14:25 -0500

    add twoSum validator

--- src/validators/array/twoSumValidator.js
+++ src/validators/array/twoSumValidator.js
@@ -1,5 +1,29 @@
-const twoSumValidator = () => {
-    return true;
-}
+const Joi = require("joi");
+
+const schema = {
+  arr: Joi.object({
+    arr: Joi.array().min(2).required().items(Joi.number())
+  }),
+  targetSum: Joi.object({
+    targetSum: Joi.number().required(),
+  }),
+};
+
+const twoSumValidator = (inputName, value) => {
+  //console.log(`twoSumValidator received inputName: ${inputName}, value: ${value}`);
+  let res = null;
+  switch (inputName) {
+      case "arr":
+        let a = value.split(",");
+        a = a.length === 1 && a[0] === "" ? [] : a;
+        res = schema[inputName].validate({ [inputName]: a});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      case "targetSum":
+        res = schema[inputName].validate({ [inputName]: value});
+        return res["error"] === undefined ? [0, res["value"][inputName]] : [1, res["error"]["message"]];
+      default:
+        return;
+  }
+};
 
 export default twoSumValidator;

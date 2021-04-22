2af493824a7b6205d9eb3cfd5ba74d01a067b418
satrancci <thescarface1993@gmail.com>
2021-04-22 16:04:55 -0500

    fix warnings

--- src/validators/array2d/sudokuSolverValidator.js
+++ src/validators/array2d/sudokuSolverValidator.js
@@ -2,7 +2,7 @@ const joi = require("joi");
 
 const schema = {
   sudokuBoard: joi.object({
-    sudokuBoard: joi.string().length(81).regex(/^[1-9\.]+$/).required(),
+    sudokuBoard: joi.string().length(81).regex(/^[1-9.]+$/).required(),
   })
 };
 
@@ -15,7 +15,7 @@ const sudokuSolverValidator = (inputName, value) => {
       case "sudokuBoard":
         res = schema[inputName].validate({ [inputName]: value});
         if (res["error"] !== undefined) {return [1, res["error"]["message"]];}
-        const arr2d = res["value"][inputName].split("").reduce((rows, key, index) => (index % 9 == 0 ? rows.push([key]) : rows[rows.length-1].push(key)) && rows, []); // one-liner from https://stackoverflow.com/questions/4492385/how-to-convert-simple-array-into-two-dimensional-array-matrix-with-javascript
+        const arr2d = res["value"][inputName].split("").reduce((rows, key, index) => (index % 9 === 0 ? rows.push([key]) : rows[rows.length-1].push(key)) && rows, []); // one-liner from https://stackoverflow.com/questions/4492385/how-to-convert-simple-array-into-two-dimensional-array-matrix-with-javascript
         return [0, arr2d];
       default:
         return;

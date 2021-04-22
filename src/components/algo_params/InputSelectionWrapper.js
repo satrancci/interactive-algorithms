76c84e2ad586951ec2c5595a430f72ca73b65ea3
satrancci <thescarface1993@gmail.com>
2021-04-22 16:18:39 -0500

    remove module import that is not used

--- src/components/algo_params/InputSelectionWrapper.js
+++ src/components/algo_params/InputSelectionWrapper.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import _ from "lodash";
-
 import InputParams from "./InputParams";
 import DebuggingState from "../debugging/DebuggingState";
 

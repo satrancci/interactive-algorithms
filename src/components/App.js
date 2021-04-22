6e257e9586b96129f82609318b629f9e9fb3c79d
satrancci <thescarface1993@gmail.com>
2021-04-22 14:23:52 -0500

    intergate App with Minimizer

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import InputDivider from "./algo_params/InputDivider";
 import VisualizationParameters from "./algo_params/VisualizationParameters";
 import Canvas from "./konva_components/Canvas";
 
+import Minimizer from "./header/Minimizer";
+
 import _ from "lodash";
 
 const initializedSize = {width: 0, height: 0}; // to avoid NaN warning
@@ -14,6 +16,9 @@ const App = (props) => {
   const [dividerSize, setDividerSize] = useState(initializedSize);
   const [visualizationParametersSize, setVisualizationParametersSize] = useState(initializedSize);
   const [canvasY, setCanvasY] = useState(0);
+  const [minimizerUp, setMinimizerUp] = useState(true);
+
+  const onMinimizerToggle = () => setMinimizerUp(!minimizerUp);
 
   const [resize, setResize] = useState(false);
 
@@ -36,10 +41,12 @@ const App = (props) => {
     setCanvasY(headerSize["height"] + dividerSize["height"] + visualizationParametersSize["height"]);
   }, [headerSize, dividerSize, visualizationParametersSize])
 
+
   return (
     <div id="app" style = {{boxSizing: "border-box", backgroundColor: "#F4F4F4"}}>
       <Header callback={onHeaderSizeUpdate} resize={resize} />
-      {props.state.algorithm ? <InputDivider callback={onDividerSizeUpdate} resize={resize}/> : null }
+      {props.state.algorithm && <Minimizer up={minimizerUp} onToggle={onMinimizerToggle}/>}
+      {props.state.algorithm && <InputDivider callback={onDividerSizeUpdate} resize={resize} displayBlock={minimizerUp}/>}
       {props.state.algorithm && (!_.isEmpty(props.state.inputObj)) && <VisualizationParameters callback={onVisualizationParametersSizeUpdate} resize={resize}/>}
       <Canvas
         x={window.innerWidth}

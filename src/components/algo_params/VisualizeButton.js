b5b58b33747c2dfd75953f08e3cc26b9ba19eb49
satrancci <thescarface1993@gmail.com>
2021-04-22 23:15:10 -0500

    simplify and also toggle isVisualizing value before and after visualization

--- src/components/algo_params/VisualizeButton.js
+++ src/components/algo_params/VisualizeButton.js
@@ -2,25 +2,17 @@ import React from 'react';
 import { connect } from "react-redux";
 import { Button } from 'semantic-ui-react';
 import algorithmMappings from '../../algorithmMappings';
-import { toggleIsVisualizing } from "../../actions";
+import { toggleIsVisualizing, updateStateAfterCancel } from "../../actions";
 
 const VisualizeButton = (props) => {
 
   const onVisualize = async () => {
-    if (!(props.state["dataStructure"] && props.state["algorithm"])) {
-      console.log("You must select ds and algorithm");
-      return;
-    }
-    console.log("onVisualize clicked!");
-    
+
     const f = algorithmMappings[props.state.algorithm].f;
     props.toggleIsVisualizing(1);
-    console.log(`IS VISUALIZING BEFORE: ${props.state.isVisualizing}`);
     await f();
     props.toggleIsVisualizing(0);
-    console.log(`IS VISUALIZING AFTER: ${props.state.isVisualizing}`);
     };
-
 
     return (
         <Button size="small" disabled={props.disabled} color="orange" style={{marginLeft: "auto"}} onClick={onVisualize}>Visualize!</Button>
@@ -36,5 +28,6 @@ const mapStateToProps = (state) => {
 
 
 export default connect(mapStateToProps, {
-  toggleIsVisualizing
+  toggleIsVisualizing,
+  updateStateAfterCancel
 })(VisualizeButton);

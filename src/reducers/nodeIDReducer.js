47ea3fe9a6dfdc24d05b740cc502c0870d54626c
satrancci <thescarface1993@gmail.com>
2021-04-22 22:53:37 -0500

    add delete nodeID action

--- src/reducers/nodeIDReducer.js
+++ src/reducers/nodeIDReducer.js
@@ -2,11 +2,10 @@ const nodeIDReducer = (nodeID=null, action) => {
     switch (action.type) {
         case 'UPDATE_NODE_ID':
           return action.payload.nodeID ? action.payload.nodeID : nodeID;
+        case 'DELETE_NODE_ID':
+          return null;
         default:
           return nodeID;
-        
-        case 'SOME_OTHER_ACTION':
-          return {}
     }
 };
 

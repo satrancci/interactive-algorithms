452f82d6f0a9f29bfb9b442a4826c0c9615bcfd9
satrancci <thescarface1993@gmail.com>
2021-04-25 14:36:02 -0500

    add GraphNode component

---
+++ src/components/konva_components/data_structures/GraphNode.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Group, Circle, Line, Text } from "react-konva";
+
+const GraphNode = (props) => {
+  
+  const {nodeID, canvasWidth, canvasHeight, radius} = props;
+  const {x, y} = props.coordinates;
+  console.log(`GraphNode received x:${x}, y:${y}, nodeID: ${JSON.stringify(nodeID)}`);
+
+  const fontSize = Math.min(Math.abs(canvasWidth), Math.abs(canvasHeight)) * 0.02;
+
+  return (
+      <Group>
+        <Circle x={x} y={y} radius={radius} fill="blue" />
+        <Text text={nodeID} x={x} y={y} fontSize={fontSize}/>
+      </Group>
+
+  );
+};
+
+export default GraphNode;

bb6a3abff599bef2cb847abc4be44060efb60c60
satrancci <thescarface1993@gmail.com>
2021-04-26 13:45:29 -0500

    improve node styling

--- src/components/konva_components/data_structures/Graph.js
+++ src/components/konva_components/data_structures/Graph.js
@@ -3,26 +3,24 @@ import { Group } from "react-konva";
 import GraphNode from "./GraphNode";
 import GraphEdge from "./GraphEdge";
 
-
 const Graph = (props) => {
-  
-  const {canvasWidth, canvasHeight, graph} = props;
+  const { canvasWidth, canvasHeight, graph } = props;
 
-  const offsetX = canvasWidth*0.05;
+  const offsetX = canvasWidth * 0.05;
 
   const positions = {
-    A: { x: offsetX, y: canvasHeight/2 },
-    B: { x: offsetX+(canvasWidth*0.1), y: canvasHeight*0.1 },
-    C: { x: offsetX+(canvasWidth*0.1), y: canvasHeight*0.9 },
-    D: { x: offsetX+(canvasWidth*0.3), y: canvasHeight*0.1 },
-    E: { x: offsetX+(canvasWidth*0.3), y: canvasHeight*0.9 },
-    F: { x: offsetX+(canvasWidth*0.5), y: canvasHeight*0.2 },
-    G: { x: offsetX+(canvasWidth*0.5), y: canvasHeight*0.8 },
-    H: { x: offsetX+(canvasWidth*0.7), y: canvasHeight*0.1 },
-    I: { x: offsetX+(canvasWidth*0.7), y: canvasHeight*0.9 },
-    J: { x: offsetX+(canvasWidth*0.9), y: canvasHeight*0.2 },
-    K: { x: offsetX+(canvasWidth*0.9), y: canvasHeight*0.8 },
-    L: { x: offsetX+(canvasWidth*0.8), y: canvasHeight/2 },
+    A: { x: offsetX, y: canvasHeight / 2 },
+    B: { x: offsetX + canvasWidth * 0.1, y: canvasHeight * 0.1 },
+    C: { x: offsetX + canvasWidth * 0.1, y: canvasHeight * 0.9 },
+    D: { x: offsetX + canvasWidth * 0.3, y: canvasHeight * 0.1 },
+    E: { x: offsetX + canvasWidth * 0.3, y: canvasHeight * 0.9 },
+    F: { x: offsetX + canvasWidth * 0.5, y: canvasHeight * 0.2 },
+    G: { x: offsetX + canvasWidth * 0.5, y: canvasHeight * 0.8 },
+    H: { x: offsetX + canvasWidth * 0.7, y: canvasHeight * 0.1 },
+    I: { x: offsetX + canvasWidth * 0.7, y: canvasHeight * 0.9 },
+    J: { x: offsetX + canvasWidth * 0.9, y: canvasHeight * 0.2 },
+    K: { x: offsetX + canvasWidth * 0.9, y: canvasHeight * 0.8 },
+    L: { x: offsetX + canvasWidth * 0.8, y: canvasHeight / 2 },
   };
 
   const radius = Math.min(Math.abs(canvasWidth), Math.abs(canvasHeight)) * 0.04;
@@ -30,9 +28,17 @@ const Graph = (props) => {
   return (
     <Group>
       {Object.entries(graph).map((edges, i) => {
+        console.log(`value: ${edges[1]["value"]}`)
         return (
           <Group key={i}>
-            <GraphNode nodeID={edges[0]} coordinates={positions[edges[0]]} canvasWidth={canvasWidth} canvasHeight={canvasHeight} radius={radius} />
+            <GraphNode
+              nodeID={edges[0]}
+              nodeValue={edges[1]["value"]}
+              coordinates={positions[edges[0]]}
+              canvasWidth={canvasWidth}
+              canvasHeight={canvasHeight}
+              radius={radius}
+            />
             {Object.entries(edges[1]["edges"]).map((edge, j) => {
               return edges[0] < edge[0] ? (
                 <GraphEdge
--- src/components/konva_components/data_structures/GraphNode.js
+++ src/components/konva_components/data_structures/GraphNode.js
@@ -3,16 +3,25 @@ import { Group, Circle, Line, Text } from "react-konva";
 
 const GraphNode = (props) => {
   
-  const {nodeID, canvasWidth, canvasHeight, radius} = props;
+  const {nodeID, canvasWidth, canvasHeight, radius, nodeValue} = props;
   const {x, y} = props.coordinates;
   //console.log(`GraphNode received x:${x}, y:${y}, nodeID: ${JSON.stringify(nodeID)}`);
 
-  const fontSize = Math.min(Math.abs(canvasWidth), Math.abs(canvasHeight)) * 0.02;
+  const nodeColor = "#7CB9E8";
+  const nodeIDTextX = x-radius*0.3;
+  const nodeIDTextY = y-radius*0.6;
+  const nodeIDFontSize = Math.min(Math.abs(canvasWidth), Math.abs(canvasHeight)) * 0.04;
+
+  const valueTextX = x-nodeValue.toString().length*0.1*radius;
+  const valueTextY = y+radius*0.45;
+  const nodeValueFontSize = Math.min(Math.abs(canvasWidth), Math.abs(canvasHeight)) * 0.02;
 
   return (
       <Group>
-        <Circle x={x} y={y} radius={radius} fill="blue" />
-        <Text text={nodeID} x={x} y={y} fontSize={fontSize}/>
+        <Circle x={x} y={y} radius={radius} fill={nodeColor} />
+        <Line points={[x-radius, y+radius*0.3, x+radius, y+radius*0.3]} stroke="black" strokeWidth={0.5}/>
+        <Text text={nodeID} x={nodeIDTextX} y={nodeIDTextY} fontSize={nodeIDFontSize}/>
+        <Text text={nodeValue} x={valueTextX} y={valueTextY} fontSize={nodeValueFontSize} />
       </Group>
 
   );

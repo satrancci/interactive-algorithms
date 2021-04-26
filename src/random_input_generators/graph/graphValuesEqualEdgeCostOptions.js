6f0c434df3693b2b908fc48a3d04685c7844a895
satrancci <thescarface1993@gmail.com>
2021-04-26 14:30:32 -0500

    change values for random input options for graph algorithms

--- src/random_input_generators/graph/graphValuesEqualEdgeCostOptions.js
+++ src/random_input_generators/graph/graphValuesEqualEdgeCostOptions.js
@@ -11,9 +11,11 @@ graph1.addEdge("B", "D", 1);
 graph1.addEdge("D", "E", 1);
 graph1.addEdge("E", "F", 1);
 graph1.addEdge("D", "F", 1);
-graph1.addEdge("F", "H", 1);
 graph1.addEdge("H", "G", 1);
 graph1.addEdge("H", "I", 1);
+graph1.addEdge("F", "G", 1);
+graph1.addEdge("E", "H", 1);
+graph1.addEdge("C", "F", 1)
 
 
 // graph2
@@ -21,12 +23,12 @@ const graph2 = new Graph();
 _.times(12, () => graph2.addNode());
 graph2.addEdge("A", "B", 1);
 graph2.addEdge("A", "C", 1);
-graph2.addEdge("B", "D", 1);
-graph2.addEdge("C", "E", 1);
-graph2.addEdge("E", "G", 1);
-graph2.addEdge("D", "F", 1);
-graph2.addEdge("F", "H", 1);
-graph2.addEdge("G", "I", 1);
+graph2.addEdge("B", "E", 1);
+graph2.addEdge("C", "F", 1);
+graph2.addEdge("E", "H", 1);
+graph2.addEdge("G", "L", 1);
+graph2.addEdge("F", "I", 1);
+graph2.addEdge("D", "G", 1);
 graph2.addEdge("H", "J", 1);
 graph2.addEdge("I", "K", 1);
 graph2.addEdge("J", "L", 1);

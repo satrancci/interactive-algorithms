55b93a4e5089e5fae004bf694796ee31ba3029cb
satrancci <thescarface1993@gmail.com>
2021-04-26 12:09:33 -0500

    handle dispplay for random input generation for graph algorithms

--- src/components/algo_params/GenerateRandomButton.js
+++ src/components/algo_params/GenerateRandomButton.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
-import { assignInputObj, updateTree } from "../../actions";
+import { assignInputObj, updateTree, updateGraph } from "../../actions";
 import inputGeneratorMappings from "../../inputGeneratorMappings";
 
 const GenerateRandomButton = (props) => {
 
     const onRandomClick = () => {
-       console.log(`onRandomClick()`);
+       //console.log(`onRandomClick()`);
        const algorithm = props.state.algorithm;
-       console.log(`algorithm: ${algorithm}`);
+       //console.log(`algorithm: ${algorithm}`);
        const inputGenerator = inputGeneratorMappings[algorithm].f;
        const randomInputObj = inputGenerator();
        props.assignInputObj(randomInputObj);
        if (randomInputObj["treeValues"]) {props.updateTree(randomInputObj["treeValues"])};
+       if (randomInputObj["graphValues"]) {props.updateGraph(randomInputObj["graphValues"])};
        props.onRandomClickCallback(randomInputObj);
     }
 
@@ -35,5 +36,6 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, {
   assignInputObj,
-  updateTree
+  updateTree,
+  updateGraph
 })(GenerateRandomButton);

8cf9804a265aff4b3c948091f6c37aa90e95095a
satrancci <thescarface1993@gmail.com>
2021-04-27 21:25:02 -0500

    sort algorithm selection alphabetically

--- src/components/ds_algo_selection/AlgorithmSelection.js
+++ src/components/ds_algo_selection/AlgorithmSelection.js
@@ -5,17 +5,11 @@ import { Dropdown } from "semantic-ui-react";
 import { updateAlgorithm, updateDataStructure, updateStateAfterCancel } from '../../actions';
 import algorithmMappings from '../../algorithmMappings';
 
-// sort by the length of algorithm name so that all options are fully visible in the dropdown
-const compare = (a, b) => {
-  if (a["key"].length > b["key"].length) {return -1;}
-  if (a["key"].length < b["key"].length) {return 1;}
-  return 0;
-}
 
 const AlgorithmSelection = (props) => {
 
   const algorithms = Object.keys(algorithmMappings);
-  const options = algorithms.map(elem => Object({key:elem, text:elem, value:elem})).sort((a,b) => compare(a,b));
+  const options = algorithms.sort().map(x => Object({key:x, text:x, value:x}));
 
   const handleOnChange = (e, data) => {
     const newDataStructure = algorithmMappings[data.value].tags[0];
@@ -36,6 +30,7 @@ const AlgorithmSelection = (props) => {
           selection
           search
           button
+          scrolling
           options={options}
         />
     </div>
